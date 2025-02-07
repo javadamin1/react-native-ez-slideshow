d2e68120e6ac97905f9291d4d6ddf3e1af524128
javad fathi <javadamin88@gmail.com>
2025-02-07 21:47:16 +0330

    fix pagination and rtl error

--- src/EzSlideshow.js
+++ src/EzSlideshow.js
@@ -1,71 +1,86 @@
-import React, { useState, useEffect, useRef } from 'react';
-import {View, FlatList, StyleSheet, Dimensions} from 'react-native';
-import SliderItem from './SliderItem';
-import Pagination  from "./Pagination";
+import React, {useState, useEffect, useRef}               from 'react';
+import {View, FlatList, StyleSheet, Dimensions, Animated} from 'react-native';
+import SliderItem                                         from './SliderItem';
+import Pagination                                         from "./Pagination";
 
 const {width} = Dimensions.get('screen');
 
 const EzSlideshow = ({
-                       images = [],
-                       autoPlay = false,
-                       duration = 3000,
-                       style = {},
-                       imageStyle = {}
-                     }) => {
+    images = [],
+    autoPlay = false,
+    duration = 3000,
+    style = {},
+    imageStyle = {}
+}) => {
 
-    if (Object.keys(images).length === 0){
+    if (Object.keys(images).length === 0) {
         return null;
     }
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const flatListRef = useRef(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const flatListRef                     = useRef(null);
+    const scrollX                         = useRef(new Animated.Value(0)).current;
 
-  useEffect(() => {
-    if (autoPlay) {
-      const interval = setInterval(() => {
-        setCurrentIndex(prevIndex =>
-          prevIndex === images.length - 1 ? 0 : prevIndex + 1
-        );
-      }, duration);
+    // AutoPlay
+    useEffect(() => {
+        if (autoPlay) {
+            const interval = setInterval(() => {
+                setCurrentIndex(prevIndex => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+            }, duration);
 
-      return () => clearInterval(interval);
-    }
-  }, [autoPlay, duration, images.length]);
-
-
-  useEffect(() => {
-    flatListRef.current?.scrollToIndex({ index: currentIndex, animated: true });
-  }, [currentIndex]);
+            return () => clearInterval(interval);
+        }
+    }, [autoPlay, duration, images.length]);
 
 
-  return (
-    <View style={[styles.container, style]}>
-      <FlatList
-        ref={flatListRef}
-        data={images}
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={({ item }) => (
-          <SliderItem item={item} parentStyles={styles} paginationIndex={currentIndex} imageStyle={imageStyle}  />
-        )}
-        //onScroll={onScrollHandler}
-      />
-      <Pagination items={images} paginationIndex={currentIndex} />
-    </View>
-  );
+    const [isListReady, setIsListReady] = useState(false);
+
+    useEffect(() => {
+        if (isListReady && flatListRef.current && images.length > 0) {
+            flatListRef.current.scrollToIndex({index: currentIndex, animated: true});
+        }
+    }, [currentIndex, images, isListReady]);
+
+
+    return (
+        <View style={[styles.container, style]}>
+            <FlatList
+                ref={flatListRef}
+                data={images}
+                horizontal
+                pagingEnabled
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={(_, index) => index.toString()}
+                renderItem={({item}) => (
+                    <SliderItem item={item} parentStyles={styles} paginationIndex={currentIndex} imageStyle={imageStyle}/>
+                )}
+                onScroll={Animated.event(
+                    [{nativeEvent: {contentOffset: {x: scrollX}}}],
+                    {useNativeDriver: false}
+                )}
+                onLayout={() => setIsListReady(true)}  // لیست آماده شد
+                scrollEventThrottle={16}
+                getItemLayout={(data, index) => ({
+                    length: width,
+                    offset: width * index,
+                    index
+                })}
+                inverted={true}
+            />
+            <Pagination items={images} scrollX={scrollX}/>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    width: width,
-  },
-  card:{
-    width: width,
-    height: 250,
-    padding:20,
-  }
+    container: {
+        width: width,
+    },
+    card     : {
+        width  : width,
+        height : 250,
+        padding: 20,
+    }
 });
 
 export default EzSlideshow;
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import {Dimensions, Image, Text, View, StyleSheet} from 'react-native';
+import { View, StyleSheet, Animated, Dimensions } from 'react-native';
 
 const {width} = Dimensions.get('screen');
 
-const Pagination = ({items,paginationIndex,scrollX}) => {
-
+const Pagination = ({ items, scrollX }) => {
   return (
     <View style={styles.container}>
       {
         items.map((_, index) => {
+        const inputRange = [(index - 1) * width, index * width, (index + 1) * width];
+
+        const dotWidth = scrollX.interpolate({
+          inputRange,
+          outputRange: [8, 16, 8],
+          extrapolate: 'clamp'
+        });
+
+        const opacity = scrollX.interpolate({
+          inputRange,
+          outputRange: [0.5, 1, 0.5],
+          extrapolate: 'clamp'
+        });
+
           return (
-            <View style={[styles.dot,{ backgroundColor: paginationIndex == index ? '#222' : '#aaa'}]}></View>
-          )
-        })
-      }
+          <Animated.View
+            key={index}
+            style={[
+              styles.dot,
+              { width: dotWidth, opacity, transform: [{ scaleX: -1 }] }
+            ]}
+          />
+        );
+      })}
     </View>
 
   );
 };
-
-export default Pagination;
 
 const styles = StyleSheet.create({
   container: {
@@ -33,9 +49,9 @@ const styles = StyleSheet.create({
   dot: {
    backgroundColor:'#aaa',
     height:8,
-    width:8,
-    marginHorizontal:2,
+    marginHorizontal: 4,
     borderRadius:8,
   },
-
 });
+
+export default Pagination;
